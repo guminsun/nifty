87545ba098a912c354c56b827917a77bf032a4d0
ceh <c.emil.hessman@gmail.com>
2011-04-06 10:32:08 +0200

    Initial distutil setup.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'nifty',
+    version = '0.1',
+    description = 'NJOY Input Format Translator',
+    install_requires = ['ply'],
+    packages = ['nifty'],
+)
