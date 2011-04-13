fb3f571a55c5120ebe1a12e8964bdf5f19d7a105
ceh <c.emil.hessman@gmail.com>
2011-04-13 18:54:30 +0200

    Fixed better organizer driver.

--- nifty/organizer/nifty_organizer.py
+++ nifty/organizer/nifty_organizer.py
@@ -1,8 +1,4 @@
 import sys
-from pprint import pprint as pprint
-
-import nifty_analyzer
-import nifty_parser
 
 ##############################################################################
 # Organizer. To put together into an orderly, functional, structured whole.
@@ -41,17 +37,3 @@ def insert_card(card, card_list):
             break
     card_list.insert(index, card)
     return card_list
-
-##############################################################################
-# Misc.
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        ast = nifty_parser.parse(open(filename).read())
-    else:
-        ast = nifty_parser.parse(sys.stdin.read())
-    ast = nifty_analyzer.analyze(ast)
-    ast = organize(ast)
-    print '--- nifty organizer output:'
-    pprint(ast)
