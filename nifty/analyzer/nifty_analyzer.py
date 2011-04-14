4397856c6d7ae27a0f309ebdaf2962dd18e3968d
ceh <c.emil.hessman@gmail.com>
2011-04-14 11:37:35 +0200

    Fixed big ugly switch.

--- nifty/analyzer/nifty_analyzer.py
+++ nifty/analyzer/nifty_analyzer.py
@@ -1,5 +1,6 @@
 import sys
 
+from analyze_acer import analyze_acer
 from analyze_reconr import analyze_reconr
 
 ##############################################################################
@@ -20,11 +21,40 @@ def analyze_module_list(module_list):
     return 'ok'
 
 def analyze_module(module):
-    # XXX: Fix big ugly switch.
-    if module['module_name'] == 'stop':
+    module_analyzers = {
+        'acer' : analyze_acer,
+        #'broadr' : analyze_broadr,
+        #'ccccr' : analyze_ccccr,
+        #'covr' : analyze_covrr,
+        #'dtfr' : analyze_dtfrr,
+        #'errorr' : analyze_errorr,
+        #'gaminr' : analyze_gaminr,
+        #'gaspr' : analyze_gaspr,
+        #'groupr' : analyze_groupr,
+        #'heatr' : analyze_heatr,
+        #'leapr' : analyze_leapr,
+        #'matxsr' : analyze_matxsr,
+        #'mixr' : analyze_mixr,
+        #'moder' : analyze_moder,
+        #'plotr' : analyze_plotr,
+        #'powr' : analyze_powr,
+        #'purr' : analyze_purr,
+        'reconr' : analyze_reconr,
+        #'resxsr' : analyze_resxsr,
+        #'thermr' : analyze_thermr,
+        #'unresr' : analyze_unresr,
+        #'viewr' : analyze_viewr,
+        #'wimsr' : analyze_wimsr,
+    }
+    module_name = module['module_name']
+    if module_name == 'stop':
         pass
-    elif module['module_name'] == 'reconr':
-        analyze_reconr(module)
     else:
-        print '--- analyzer: XXX not implemented yet:', module['module_name']
+        try:
+            analyze = module_analyzers[module_name]
+            analyze(module)
+        except KeyError:
+            msg = ('--- nifty_analyzer: XXX not implemented yet: ' +
+                   module_name)
+            print msg
     return 'ok'
