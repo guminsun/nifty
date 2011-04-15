ec42392a55724d901eb70de87c98f2d3a62e254f
ceh <c.emil.hessman@gmail.com>
2011-04-15 08:14:57 +0200

    Cleaned up. Use a for directive instead of listing a rule for each identifier.

--- nifty/analyzer/analyze_acer.py
+++ nifty/analyzer/analyze_acer.py
@@ -30,28 +30,10 @@ def analyze_acer_card_1(card_1, module):
                       card_id = (1, '').
     '''
     statement_list = card_1['statement_list']
-
     must_be_defined = ['nendf', 'npend', 'ngend', 'nace', 'ndir']
     rule.identifiers_must_be_defined(must_be_defined, card_1, module)
-
-    nendf = helper.get_identifier('nendf', statement_list)
-    rule.identifier_must_be_int(nendf)
-    rule.identifier_must_be_unit_number(nendf)
-
-    npend = helper.get_identifier('npend', statement_list)
-    rule.identifier_must_be_int(npend)
-    rule.identifier_must_be_unit_number(npend)
-
-    ngend = helper.get_identifier('ngend', statement_list)
-    rule.identifier_must_be_int(ngend)
-    rule.identifier_must_be_unit_number(ngend)
-
-    nace = helper.get_identifier('nace', statement_list)
-    rule.identifier_must_be_int(nace)
-    rule.identifier_must_be_unit_number(nace)
-
-    ndir = helper.get_identifier('ndir', statement_list)
-    rule.identifier_must_be_int(ndir)
-    rule.identifier_must_be_unit_number(ndir)
-
+    for identifier in must_be_defined:
+        id_node = helper.get_identifier(identifier, statement_list)
+        rule.identifier_must_be_int(id_node)
+        rule.identifier_must_be_unit_number(id_node)
     return 'ok'
