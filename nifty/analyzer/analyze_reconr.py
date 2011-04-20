38d6244ddf47def808e29941c80380cd10f5e0c6
ceh <c.emil.hessman@gmail.com>
2011-04-20 06:29:37 +0200

    Added new outline for card_3, module reconr.

--- nifty/analyzer/analyze_reconr.py
+++ nifty/analyzer/analyze_reconr.py
@@ -29,49 +29,57 @@ def analyze_reconr_card_list(module):
     #
     # Cards 3, 4, 5, 6 must be input for each material desired.
     cards_3 = helper.get_cards('card_3', module)
-    for card_3 in cards_3:
-        analyze_reconr_card_3(card_3, module)
 
-        # Break loop if mat = 0, which indicates termination of execution.
-        mat = helper.get_identifier('mat', card_3)
-        mat_value = helper.get_value(helper.get_r_value(mat))
-        if mat_value == 0:
-            break
+    # XXX: length of 4, 5, 6 must match length of 3.
+    cards_4 = helper.get_cards('card_4', module)
+    cards_5 = helper.get_cards('card_5', module)
+    cards_6 = helper.get_cards('card_6', module)
 
-        card_4 = helper.get_card('card_4', module)
-        analyze_reconr_card_4(card_4, module)
+    number_of_card_3 = len(cards_3)
+    for i in range(number_of_card_3):
+        print i
+        analyze_reconr_card_3(cards_3[i], module)
 
-        # card_5 must be defined 'ncards' times for each card_3 (see card 3).
-        ncards = helper.get_identifier('ncards', card_3)
-        if helper.not_defined(ncards):
-            ncards_value = 0
-        else:
-            ncards_value = helper.get_value(helper.get_r_value(ncards))
-        cards_5 = helper.get_cards('card_5', module)
-        if len(cards_5) != ncards_value:
-            # Number of card_5 does not match ncards definition in card 3.
-            msg = ('\'card_5\' declared ' + str(len(cards_5)) + ' time(s) ' +
-                   'while \'ncards\' is set to ' + str(ncards_value) +
-                   ' in \'card_3\', module \'reconr\'.')
-            rule.semantic_error(msg, module)
-        for card_5 in cards_5:
-            analyze_reconr_card_5(card_5, module)
+        ## Break loop if mat = 0, which indicates termination of execution.
+        #mat = helper.get_identifier('mat', card_3)
+        #mat_value = helper.get_value(helper.get_r_value(mat))
+        #if mat_value == 0:
+        #    break
 
-        # card_6 must be defined 'ngrid' times for each card_3 (see card 3).
-        ngrid = helper.get_identifier('ngrid', card_3)
-        if helper.not_defined(ngrid):
-            ngrid_value = 0
-        else:
-            ngrid_value = helper.get_value(helper.get_r_value(ngrid))
-        cards_6 = helper.get_cards('card_6', module)
-        if len(cards_6) != ngrid_value:
-            # Number of card_6 does not match ncards definition in card 3.
-            msg = ('\'card_6\' declared ' + str(len(cards_6)) + ' time(s) ' +
-                   'while \'ngrid\' is set to ' + str(ngrid_value) +
-                   ' in \'card_3\', module \'reconr\'.')
-            rule.semantic_error(msg, module)
-        for card_6 in cards_6:
-            analyze_reconr_card_6(card_6, module)
+        #card_4 = helper.get_card('card_4', module)
+        #analyze_reconr_card_4(card_4, module)
+        #
+        ## card_5 must be defined 'ncards' times for each card_3 (see card 3).
+        #ncards = helper.get_identifier('ncards', card_3)
+        #if helper.not_defined(ncards):
+        #    ncards_value = 0
+        #else:
+        #    ncards_value = helper.get_value(helper.get_r_value(ncards))
+        #cards_5 = helper.get_cards('card_5', module)
+        #if len(cards_5) != ncards_value:
+        #    # Number of card_5 does not match ncards definition in card 3.
+        #    msg = ('\'card_5\' declared ' + str(len(cards_5)) + ' time(s) ' +
+        #           'while \'ncards\' is set to ' + str(ncards_value) +
+        #           ' in \'card_3\', module \'reconr\'.')
+        #    rule.semantic_error(msg, module)
+        #for card_5 in cards_5:
+        #    analyze_reconr_card_5(card_5, module)
+        #
+        ## card_6 must be defined 'ngrid' times for each card_3 (see card 3).
+        #ngrid = helper.get_identifier('ngrid', card_3)
+        #if helper.not_defined(ngrid):
+        #    ngrid_value = 0
+        #else:
+        #    ngrid_value = helper.get_value(helper.get_r_value(ngrid))
+        #cards_6 = helper.get_cards('card_6', module)
+        #if len(cards_6) != ngrid_value:
+        #    # Number of card_6 does not match ncards definition in card 3.
+        #    msg = ('\'card_6\' declared ' + str(len(cards_6)) + ' time(s) ' +
+        #           'while \'ngrid\' is set to ' + str(ngrid_value) +
+        #           ' in \'card_3\', module \'reconr\'.')
+        #    rule.semantic_error(msg, module)
+        #for card_6 in cards_6:
+        #    analyze_reconr_card_6(card_6, module)
 
     return 'ok'
 
@@ -117,13 +125,15 @@ def analyze_reconr_card_3(card_3, module):
 def analyze_reconr_card_4(card_4, module):
     err_node = rule.identifier_must_be_defined('err', card_4, module)
     rule.identifier_must_be_float(err_node)
+    # XXX: Check tempr, errmax, errint if they are defined.
     return 'ok'
 
 def analyze_reconr_card_5(card_5, module):
-    cards = rule.identifier_must_be_defined('cards', card_5, module)
-    rule.identifier_must_be_string(cards, card_5, module)
+    # XXX: Check that the number of cards's corresponds to the ncards value
+    #      defined in card_3
     return 'ok'
 
 def analyze_reconr_card_6(card_6, module):
-    rule.identifier_must_be_defined('enode', card_6, module)
+    # XXX: Check that the number of enode's corresponds to the ngrid value
+    #      defined in card_3
     return 'ok'
