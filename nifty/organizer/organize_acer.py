ff10ddd17e334604ef5b3344cb7f59a213c41127
ceh <c.emil.hessman@gmail.com>
2011-04-20 14:17:33 +0200

    Added import of helpers.

--- nifty/organizer/organize_acer.py
+++ nifty/organizer/organize_acer.py
@@ -1,3 +1,5 @@
+from nifty.env import nifty_helpers as helper
+
 ##############################################################################
 # Organize acer. Put together into an orderly, functional, structured whole.
 
