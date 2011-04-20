9a7670ab4e9a586cc7a9f9e5111f41e1e7f73226
ceh <c.emil.hessman@gmail.com>
2011-04-20 15:12:16 +0200

    Added possible structure of how to organize cards.

--- nifty/organizer/organize_acer.py
+++ nifty/organizer/organize_acer.py
@@ -3,10 +3,32 @@ from nifty.environment import helpers as helper
 ##############################################################################
 # Organize acer. Put together into an orderly, functional, structured whole.
 
-def organize_acer(acer_module_node):
-    acer_card_list = helper.get_card_list(acer_module_node)
-    organize_acer_card_list(acer_card_list)
-    return acer_module_node
+def organize_acer(module):
+    card_list = helper.get_card_list(module)
+    organize_acer_card_list(card_list, module)
+    return module
 
-def organize_acer_card_list(acer_card_list):
-    return acer_card_list
+def organize_acer_card_list(card_list, module):
+    for card in card_list:
+        organize_card(card)
+    return card_list
+
+def organize_card(card):
+    organizer_function = organizer_function_dummy
+    card_functions = {
+        'card_1' : organize_card_1,
+    }
+    card_name = helper.get_card_name(card)
+    try:
+        organizer_function = card_functions[card_name]
+    except KeyError:
+        msg = ('--- organize_acer: XXX not implemented yet: ' + card_name)
+        print msg
+    organizer_function(card)
+    return card
+
+def organizer_function_dummy(card):
+    pass
+
+def organize_card_1(card):
+    pass
