755a8d4d19952084b71319120b88eab608ad620f
ceh <c.emil.hessman@gmail.com>
2011-04-21 18:30:55 +0200

    Documented sort_statement_list and fixed its return values.

--- nifty/organizer/organizer_helpers.py
+++ nifty/organizer/organizer_helpers.py
@@ -1,20 +1,42 @@
 from nifty.environment import helpers as env
 
+##############################################################################
+# Organizer helpers.
+
 def sort_statement_list(ordered_id_names, card):
-    # 'ordered_id_names' specifies in which order the identifiers should be
-    # ordered in 'card'.
+    '''
+        Return a statement list where all identifiers in 'card's statement
+        list has been sorted according to the order of the corresponding
+        identifier names in 'ordered_id_names'.
+
+        Use this function to sort statement lists where all identifiers must
+        be provided.
+
+        For example, if ordered_id_names = ['one', 'two'] and
+        card['statement_list'] = ['two', 'one'] then
+        statement_list = ['one', 'two'] will be returned.
+
+        If the number of defined identifiers in the card's statement list are
+        more than the number of names given in 'ordered_id_names', then the
+        original statement list will be returned.
+
+        If any of the names given in 'ordered_id_names' is not defined in the
+        card's statement list, then the original statement list will be
+        returned.
+    '''
     statement_list = env.get_statement_list(card)
+    # If the number of expected identifiers is less than the number of defined
+    # identifiers, then return the original statement list such that the
+    # analyzer can report any semantical errors.
+    if len(ordered_id_names) < len(statement_list):
+        return statement_list
     new_statement_list = list()
     for id_name in ordered_id_names:
         node = env.get_identifier(id_name, card)
         if env.not_defined(node):
-            # XXX Insert default value.
-            pass
+            # All identifiers must be defined. Return the original statement
+            # list such that the analyzer can report any semantical errors.
+            return statement_list
         else:
             new_statement_list.append(node)
-    # If the new list differs from the original, return the original such that
-    # the analyzer can report any semantical errors to the user.
-    if len(new_statement_list) != len(statement_list):
-        return statement_list
-    else:
-        return new_statement_list
+    return new_statement_list
