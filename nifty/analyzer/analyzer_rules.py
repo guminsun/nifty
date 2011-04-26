e775ec2c303b6699d399e33048fc31e66da52b95
ceh <c.emil.hessman@gmail.com>
2011-04-26 18:29:39 +0200

    Cleaned up. Restructured order.

--- nifty/analyzer/analyzer_rules.py
+++ nifty/analyzer/analyzer_rules.py
@@ -5,14 +5,6 @@ from nifty.environment import helpers as env
 ##############################################################################
 # Semantic rules.
 
-def cards_must_be_defined(must_be_defined, module_node):
-    cards = list()
-    for card_name in must_be_defined:
-        msg = ('i.e. expected a declaration of \'' + card_name + '\'')
-        c = card_must_be_defined(card_name, module_node, msg)
-        cards.append(c)
-    return cards
-
 def card_must_be_defined(card_name, module_node, msg):
     card_node = env.get_card(card_name, module_node)
     if card_node is None:
@@ -29,10 +21,12 @@ def card_must_not_be_defined(card_name, module_node, msg):
         semantic_error(msg, card_node)
     return card_node
 
-def cards_must_be_unique(unique_card_list, module_node):
-    for card_name in unique_card_list:
-        cards = env.get_cards(card_name, module_node)
-        card_must_be_unique(card_name, module_node)
+def cards_must_be_defined(must_be_defined, module_node):
+    cards = list()
+    for card_name in must_be_defined:
+        msg = ('i.e. expected a declaration of \'' + card_name + '\'')
+        c = card_must_be_defined(card_name, module_node, msg)
+        cards.append(c)
     return cards
 
 def card_must_be_unique(card_name, module_node):
@@ -45,45 +39,11 @@ def card_must_be_unique(card_name, module_node):
         semantic_error(msg, card_node)
     return card_node
 
-def number_of_cards_must_be(number, card_name_1, card_name_2, module):
-    '''
-        Return 'ok' if the number of cards for 'card_name_1' is equal to
-        'number', else a semantic error indicating the ratio mismatch.
-    '''
-    card_list_1 = env.get_cards(card_name_1, module)
-    card_len_1 = len(card_list_1)
-    if card_len_1 != number:
-        module_name = env.get_module_name(module)
-        msg = ('in module \'' + module_name + '\': card \'' + card_name_1 +
-               '\' is declared ' + str(card_len_1) + ' time(s) while card \'' +
-               card_name_2 + '\' has been ' + 'declared ' + str(number) +
-               ' time(s), expected a 1:1 ratio.')
-        semantic_error(msg, module)
-    return 'ok'
-
-def node_must_be_identifier(l_value_node, card_node, module_node):
-    if env.is_identifier(l_value_node):
-        return l_value_node
-    else:
-        id_name = env.get_identifier_name(l_value_node)
-        node_type = env.get_node_type(l_value_node)
-        msg = ('identifier \'' + id_name + '\' defined as an ' + node_type +
-               ' in \'' + card_node['card_name'] + '\', module \'' +
-               module_node['module_name'] + '\' (expected a regular ' +
-               'identifier declaration).')
-        semantic_error(msg, l_value_node)
-
-def node_must_be_array(l_value_node, card_node, module_node):
-    if env.is_array(l_value_node):
-        return l_value_node
-    else:
-        id_name = env.get_identifier_name(l_value_node)
-        node_type = env.get_node_type(l_value_node)
-        msg = ('identifier \'' + id_name + '\' defined as an ' + node_type +
-               ' in \'' + card_node['card_name'] + '\', module \'' +
-               module_node['module_name'] + '\' (expected an array ' +
-               'identifier declaration).')
-        semantic_error(msg, l_value_node)
+def cards_must_be_unique(unique_card_list, module_node):
+    for card_name in unique_card_list:
+        cards = env.get_cards(card_name, module_node)
+        card_must_be_unique(card_name, module_node)
+    return cards
 
 def identifier_must_be_defined(name_index, node, card_node, module_node):
     id_name = name_index[0]
@@ -103,7 +63,7 @@ def identifier_must_be_defined(name_index, node, card_node, module_node):
 
     if not env.is_valid_name(name, id_name):
         msg = ('expected identifier \'' + id_name + '\' but saw \'' +
-               name + '\' in \'' + card_node['card_name'] + 
+               name + '\' in \'' + card_node['card_name'] +
                '\', module \'' + module_node['module_name'] + '.')
         semantic_error(msg, l_value_node)
 
@@ -176,6 +136,30 @@ def identifier_string_must_not_exceed_length(id_node, max_length, card_node,
         semantic_error(msg, id_node)
     return 'ok'
 
+def node_must_be_array(l_value_node, card_node, module_node):
+    if env.is_array(l_value_node):
+        return l_value_node
+    else:
+        id_name = env.get_identifier_name(l_value_node)
+        node_type = env.get_node_type(l_value_node)
+        msg = ('identifier \'' + id_name + '\' defined as an ' + node_type +
+               ' in \'' + card_node['card_name'] + '\', module \'' +
+               module_node['module_name'] + '\' (expected an array ' +
+               'identifier declaration).')
+        semantic_error(msg, l_value_node)
+
+def node_must_be_identifier(l_value_node, card_node, module_node):
+    if env.is_identifier(l_value_node):
+        return l_value_node
+    else:
+        id_name = env.get_identifier_name(l_value_node)
+        node_type = env.get_node_type(l_value_node)
+        msg = ('identifier \'' + id_name + '\' defined as an ' + node_type +
+               ' in \'' + card_node['card_name'] + '\', module \'' +
+               module_node['module_name'] + '\' (expected a regular ' +
+               'identifier declaration).')
+        semantic_error(msg, l_value_node)
+
 def no_more_statement_allowed(node, card_node, module_node):
     if not env.not_defined(node):
         node_name = env.get_identifier_name(env.get_l_value(node))
@@ -183,10 +167,26 @@ def no_more_statement_allowed(node, card_node, module_node):
         stmt = node_name + ' = ' + str(node_value)
         card_name = card_node['card_name']
         module_name = module_node['module_name']
-        msg = ('unexpected statement: \'' + stmt + '\', no more ' + 
+        msg = ('unexpected statement: \'' + stmt + '\', no more ' +
                'statements was expected in \'' + card_name + '\', module ' +
                '\'' + module_name + '\'.')
         semantic_error(msg, node)
+
+def number_of_cards_must_be(number, card_name_1, card_name_2, module):
+    '''
+        Return 'number' if the number of cards for 'card_name_1' is equal to
+        'number', else a semantic error indicating the ratio mismatch.
+    '''
+    card_list_1 = env.get_cards(card_name_1, module)
+    card_len_1 = len(card_list_1)
+    if card_len_1 != number:
+        module_name = env.get_module_name(module)
+        msg = ('in module \'' + module_name + '\': card \'' + card_name_1 +
+               '\' is declared ' + str(card_len_1) + ' time(s) while card \'' +
+               card_name_2 + '\' has been ' + 'declared ' + str(number) +
+               ' time(s), expected a 1:1 ratio.')
+        semantic_error(msg, module)
+    return number
 
 ##############################################################################
 # Error handling.
