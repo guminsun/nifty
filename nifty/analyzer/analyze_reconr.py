eb95863d8d39fead1ae4f0045025b4b81d5e3562
ceh <c.emil.hessman@gmail.com>
2011-04-28 16:19:10 +0200

    Restructured the analyzer for module reconr.

--- nifty/analyzer/analyze_reconr.py
+++ nifty/analyzer/analyze_reconr.py
@@ -9,115 +9,62 @@ def analyze_reconr(module):
     return module
 
 def analyze_reconr_card_list(module):
-    # Check for cards that always must be defined.
-    must_be_defined = ['card_1', 'card_2', 'card_3', 'card_4']
-    rule.cards_must_be_defined(must_be_defined, module)
-
-    # Check for cards that must be unique (e.g. not defined more than once).
-    unique_card_list = ['card_1', 'card_2']
-    rule.cards_must_be_unique(unique_card_list, module)
-
-    card_1 = env.get_card('card_1', module)
-    analyze_reconr_card_1(card_1, module)
-
-    card_2 = env.get_card('card_2', module)
-    analyze_reconr_card_2(card_2, module)
-
-    card_list_3 = env.get_cards('card_3', module)
-    card_list_4 = env.get_cards('card_4', module)
-    card_list_5 = env.get_cards('card_5', module)
-    card_list_6 = env.get_cards('card_6', module)
-
-    # Cards 4 must be input for each material desired (card 3), make a simple
-    # check to see if the number of cards is a 1:1 ratio. Note that the last
-    # card_3, where mat = 0, should not be considered.
-    cards_3_len = len(card_list_3)-1
-    rule.number_of_cards_must_be(cards_3_len, 'card_4', 'card_3', module)
-
-    # XXX: Ugly.
-    for i in range(len(card_list_3)):
-
-        analyze_reconr_card_3(card_list_3[i], module)
-        # Skip the checks of card 4,5,6 if the current 'card_3' is the last.
-        # I.e. break loop if mat = 0, which indicate termination of execution.
-        mat_node = env.get_identifier('mat', card_list_3[i])
-        mat_r_value = env.get_value(env.get_r_value(mat_node))
-        if mat_r_value == 0:
-            break
-
-#        analyze_reconr_card_4(card_list_4[i], module)
-#
-#        # XXX: Ugly.
-#        # Note that card_5 should only be defined if ncards > 0 in card_3.
-#        ncards_node = env.get_identifier('ncards', card_list_3[i])
-#        if env.not_defined(ncards_node):
-#            ncards_r_value = 0
-#        else:
-#            ncards_r_value = env.get_value(env.get_r_value(ncards_node))
-#        c5_start = i*ncards_r_value
-#        c5_end = c5_start+ncards_r_value
-#        if ncards_r_value > 0:
-#            # XXX: Check that the number of cards are correct? Assume they are
-#            #      for now.
-#            # Extract the correct card_5(s) to analyze.
-#            for j in range(c5_start, c5_end):
-#                # XXX: Ugly. 'card_list_5[j]' must be defined.
-#                try:
-#                    analyze_reconr_card_5(card_list_5[j], module)
-#                except:
-#                    msg = ('expected a \'card_5\' since ncards = ' +
-#                           str(ncards_r_value) + ' in \'card_3\', module ' +
-#                           '\'' + env.get_module_name(module) + '\'.')
-#                    rule.semantic_error(msg, card_list_3[i])
-#        else:
-#            if len(card_list_5) > c5_start:
-#                msg = ('unexpected definition of card \'card_5\' in module \'' +
-#                       env.get_module_name(module) + '\' (since ncards = ' +
-#                       str(ncards_r_value) + ' in \'card_3\').')
-#                rule.semantic_error(msg, card_list_3[i])
-#
-#        # XXX: Ugly.
-#        # Note that card_6 should only be defined if ngrid > 0 in card_3.
-#        ngrid_node = env.get_identifier('ngrid', card_list_3[i])
-#        if env.not_defined(ngrid_node):
-#            ngrid_r_value = 0
-#        else:
-#            ngrid_r_value = env.get_value(env.get_r_value(ngrid_node))
-#        c6_start = i*ngrid_r_value
-#        c6_end = c6_start+ngrid_r_value
-#        if ngrid_r_value > 0:
-#            # XXX: Check that the number of cards are correct? Assume they are
-#            #      for now.
-#            # Extract the correct card_6(s) to analyze.
-#            for j in range(c6_start, c6_end):
-#                # XXX: Ugly. 'card_list_6[j]' must be defined.
-#                try:
-#                    analyze_reconr_card_6(card_list_6[j], module)
-#                except:
-#                    msg = ('expected a \'card_6\' since ngrid = ' +
-#                           str(ngrid_r_value) + ' in \'card_3\', module ' +
-#                           '\'' + env.get_module_name(module) + '\'.')
-#                    rule.semantic_error(msg, card_list_3[i])
-#        else:
-#            if len(card_list_6) > c6_start:
-#                msg = ('unexpected definition of card \'card_6\' in module \'' +
-#                       env.get_module_name(module) + '\' (since ngrid = ' +
-#                       str(ngrid_r_value) + ' in \'card_3\').')
-#                rule.semantic_error(msg, card_list_3[i])
-#
+    # Use a card iterator to check whether the cards have been defined in the
+    # expected order.
+    card_iter = env.get_card_iterator(module)
+    # Card 1 must always be defined.
+    analyze_reconr_card_1(env.next(card_iter), module)
+    # Card 2 must always be defined.
+    analyze_reconr_card_2(env.next(card_iter), module)
+    # Number of card_3's should at least be 2 since one card 3 must always be
+    # supplied, and there must be an ending card 3 (with mat = 0) to indicate 
+    # termination of reconr.
+    number_of_card_3 = len(env.get_cards('card_3', module))
+    if number_of_card_3 < 2:
+        rule.too_few_cards_defined(number_of_card_3, 2, 'card_3', module)
+    # The last card 3 should not be considered, since it is expected to
+    # terminate the execution of reconr (i.e. mat = 0), therefore,
+    # 'number_of_card_3-1' is used to create the range to iterate over.
+    for c3 in range(number_of_card_3-1):
+        # Card 3 must always be defined.
+        card_3, ncards, ngrid = analyze_reconr_card_3(env.next(card_iter), module)
+        # Card 4 must be defined for each material desired (card 3).
+        analyze_reconr_card_4(env.next(card_iter), module)
+        # Card 5 must be defined 'ncards' number of times.
+        for c5 in range(ncards):
+            analyze_reconr_card_5(ncards, env.next(card_iter), module)
+        # XXX Not sure about this; assuming that it works in the same way as
+        #     for card 5:
+        # Card 6 must be defined 'ngrid' number of times.
+        for c6 in range(ngrid):
+            analyze_reconr_card_6(ngrid, env.next(card_iter), module)
+    # The last card is expected to be a card 3 with mat = 0, to indicate
+    # termination of reconr.
+    analyze_reconr_card_3_stop(env.next(card_iter), module)
+    # No more cards are allowed. The next card returned by env.next(card_iter)
+    # should be 'None'.
+    rule.no_card_allowed(env.next(card_iter), module)
     return module
 
 def analyze_reconr_card_1(card_1, module):
+    # Card 1 must be defined.
+    rule.card_must_be_defined('card_1', card_1, module, None)
+    # Use a statement iterator to check whether the identifiers have been
+    # defined in the expected order.
     stmt_iter = env.get_statement_iterator(card_1)
     rule.analyze_identifier_nendf(env.next(stmt_iter), card_1, module)
     rule.analyze_identifier_npend(env.next(stmt_iter), card_1, module)
-    rule.no_more_statement_allowed(env.next(stmt_iter), card_1, module)
+    # No more statements are allowed. The next statement returned by
+    # env.next(card_iter) should be 'None'.
+    rule.no_statement_allowed(env.next(stmt_iter), card_1, module)
     return card_1
 
 def analyze_reconr_card_2(card_2, module):
+    # Card 2 must be defined.
+    rule.card_must_be_defined('card_2', card_2, module, None)
     stmt_iter = env.get_statement_iterator(card_2)
     analyze_reconr_card_2_tlabel(env.next(stmt_iter), card_2, module)
-    rule.no_more_statement_allowed(env.next(stmt_iter), card_2, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card_2, module)
     return card_2
 
 def analyze_reconr_card_2_tlabel(tlabel_node, card_2, module):
@@ -126,58 +73,133 @@ def analyze_reconr_card_2_tlabel(tlabel_node, card_2, module):
     rule.identifier_must_be_string(tlabel_node, card_2, module)
     rule.identifier_string_must_not_exceed_length(tlabel_node, 66, card_2,
                                                   module)
-    return tlabel_node
+    return env.get_value(env.get_r_value(tlabel_node))
 
 def analyze_reconr_card_3(card_3, module):
+    # Card 3 must be defined.
+    rule.card_must_be_defined('card_3', card_3, module, None)
     stmt_iter = env.get_statement_iterator(card_3)
-    analyze_reconr_card_3_mat(env.next(stmt_iter), card_3, module)
-    analyze_reconr_card_3_ncards(env.next(stmt_iter), card_3, module)
-    analyze_reconr_card_3_ngrid(env.next(stmt_iter), card_3, module)
-    rule.no_more_statement_allowed(env.next(stmt_iter), card_3, module)
-    return card_3
-
-def analyze_reconr_card_3_mat(mat_node, card_3, module):
-    rule.identifier_must_be_defined(('mat', None), mat_node, card_3, module)
-    return mat_node
+    rule.analyze_identifier_matd(env.next(stmt_iter), card_3, module)
+    ncards = analyze_reconr_card_3_ncards(env.next(stmt_iter), card_3, module)
+    ngrid = analyze_reconr_card_3_ngrid(env.next(stmt_iter), card_3, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card_3, module)
+    return card_3, ncards, ngrid
 
 def analyze_reconr_card_3_ncards(ncards_node, card_3, module):
     # ncards does not have to be defined, defaults to 0.
     if env.not_defined(ncards_node):
-        return ncards_node
+        return 0
     else:
         rule.identifier_must_be_defined(('ncards', None), ncards_node, card_3,
                                         module)
         rule.identifier_must_be_int(ncards_node)
         # XXX: Check if ncards is positive, negative number of cards is not a
         #      proper input.
-    return ncards_node
+    return env.get_value(env.get_r_value(ncards_node))
 
 def analyze_reconr_card_3_ngrid(ngrid_node, card_3, module):
     # ngrid does not have to be defined, defaults to 0.
     if env.not_defined(ngrid_node):
-        return ngrid_node
+        return 0
     else:
         rule.identifier_must_be_defined(('ngrid', None), ngrid_node, card_3,
                                         module)
+        rule.identifier_must_be_int(ngrid_node)
         # XXX: Check if ngrid is positive, negative number of grids is not a
         #      proper input.
-    return ngrid_node
+    return env.get_value(env.get_r_value(ngrid_node))
+
+def analyze_reconr_card_3_stop(card_3, module):
+    # Card 3 with mat = 0 must be defined.
+    rule.card_must_be_defined('card_3', card_3, module, None)
+    stmt_iter = env.get_statement_iterator(card_3)
+    mat = rule.analyze_identifier_matd(env.next(stmt_iter), card_3, module)
+    # The last card is expected to be a card 3 with mat = 0, to indicate
+    # termination of reconr.
+    if mat != 0:
+        msg = ('expected a \'card_3\' with the material set to 0 to ' + 
+               'indicate termination of module \'reconr\'.')
+        rule.semantic_error(msg, card_3)
+    rule.no_statement_allowed(env.next(stmt_iter), card_3, module)
+    return mat
 
 def analyze_reconr_card_4(card_4, module):
-    err_node = rule.identifier_must_be_defined('err', card_4, module)
-    rule.identifier_must_be_float(err_node)
-    # XXX: Check tempr, errmax, errint if they are defined.
+    msg = ('\'card_4\' must be defined for each material desired (card 3)')
+    rule.card_must_be_defined('card_4', card_4, module, msg)
+    stmt_iter = env.get_statement_iterator(card_4)
+    err = analyze_reconr_card_4_err(env.next(stmt_iter), card_4, module)
+    analyze_reconr_card_4_tempr(env.next(stmt_iter), card_4, module)
+    analyze_reconr_card_4_errmax(err, env.next(stmt_iter), card_4, module)
+    analyze_reconr_card_4_errint(err, env.next(stmt_iter), card_4, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card_4, module)
     return card_4
 
-def analyze_reconr_card_5(card_5, module):
-    analyze_reconr_card_5_cards(card_5, module)
+def analyze_reconr_card_4_err(err_node, card_4, module):
+    # Fractional reconstruction tolerance used when resonance-integral error
+    # criterion (see errint) is not satisfied.
+    rule.identifier_must_be_defined(('err', None), err_node, card_4, module)
+    rule.identifier_must_be_float(err_node)
+    return env.get_value(env.get_r_value(err_node))
+
+def analyze_reconr_card_4_tempr(tempr_node, card_4, module):
+    # Reconstruction temperature (degree Kelvin).
+    # tempr does not have to be defined, defaults to 0.
+    if env.not_defined(tempr_node):
+        return 0
+    else:
+        rule.identifier_must_be_defined(('tempr', None), tempr_node, card_4,
+                                        module)
+    return env.get_value(env.get_r_value(tempr_node))
+
+def analyze_reconr_card_4_errmax(err_value, errmax_node, card_4, module):
+    # Fractional reconstruction tolerance used when resonance-integral error
+    # criterion is satisfied.
+    # errmax does not have to be defined, defaults to 10*err.
+    if env.not_defined(errmax_node):
+        return 10*err_value
+    else:
+        rule.identifier_must_be_defined(('errmax', None), errmax_node, card_4,
+                                        module)
+        # XXX: criterion: errmax_value >= err_value
+    return env.get_value(env.get_r_value(errmax_node))
+
+def analyze_reconr_card_4_errint(err_value, errint_node, card_4, module):
+    # Maximum resonance-integral error (in barns) per grid point.
+    # errint does not have to be defined, defaults to err/20000.
+    if env.not_defined(errint_node):
+        # XXX: Will the Python float definition introduce accuracy problems?
+        #      Just a heads up if this return value will be used in the
+        #      translator.
+        return float(err_value)/20000
+    else:
+        rule.identifier_must_be_defined(('errint', None), errint_node, card_4,
+                                        module)
+    return env.get_value(env.get_r_value(errint_node))
+
+def analyze_reconr_card_5(ncards_value, card_5, module):
+    msg = ('expected ' + str(ncards_value) + ' \'card_5\'s, since ' +
+           'ncards = ' + str(ncards_value) + ' in the current \'card_3\'.')
+    rule.card_must_be_defined('card_5', card_5, module, msg)
+    stmt_iter = env.get_statement_iterator(card_5)
+    analyze_reconr_card_5_cards(env.next(stmt_iter), card_5, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card_5, module)
     return card_5
 
-def analyze_reconr_card_5_cards(card_5, module):
-    cards_node = rule.identifier_must_be_defined('cards', card_5, module)
+def analyze_reconr_card_5_cards(cards_node, card_5, module):
+    rule.identifier_must_be_defined(('cards', None), cards_node, card_5, module)
     rule.identifier_must_be_string(cards_node, card_5, module)
+    return env.get_value(env.get_r_value(cards_node))
 
-def analyze_reconr_card_6(card_6, module):
-    # XXX: Check that the number of enode's corresponds to the ngrid value
-    #      defined in card_3
+def analyze_reconr_card_6(ngrid_value, card_6, module):
+    msg = ('expected ' + str(ngrid_value) + ' \'card_6\'s, since ' +
+           'ngrid = ' + str(ngrid_value) + ' in the current \'card_3\'.')
+    rule.card_must_be_defined('card_6', card_6, module, msg)
+    stmt_iter = env.get_statement_iterator(card_6)
+    analyze_reconr_card_6_enode(env.next(stmt_iter), card_6, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card_6, module)
     return card_6
+
+def analyze_reconr_card_6_enode(enode_node, card_6, module):
+    rule.identifier_must_be_defined(('enode', None), enode_node, card_6, module)
+    # XXX: Typecheck?
+    return env.get_value(env.get_r_value(enode_node))
