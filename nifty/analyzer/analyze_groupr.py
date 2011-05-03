c30ba8c90a6158f6ec5858d11f6db7cc46700cf5
ceh <c.emil.hessman@gmail.com>
2011-05-03 15:27:48 +0200

    Added semantic checks for card 5, module groupr.

--- nifty/analyzer/analyze_groupr.py
+++ nifty/analyzer/analyze_groupr.py
@@ -18,6 +18,8 @@ def analyze_groupr_card_list(module):
     analyze_groupr_card_3(env.next(card_iter), module)
     # Card 4 must always be defined.
     analyze_groupr_card_4(ntemp, env.next(card_iter), module)
+    # Card 5 must always be defined.
+    analyze_groupr_card_5(nsigz, env.next(card_iter), module)
 
     # No more cards are allowed. The next card returned by env.next(card_iter)
     # should be 'None'.
@@ -138,3 +140,25 @@ def analyze_groupr_card_4_temp(expected_index, temp_node, card_4, module):
     expected = ('temp', expected_index)
     rule.identifier_must_be_defined(expected, temp_node, card_4, module)
     return env.get_value(env.get_r_value(temp_node))
+
+def analyze_groupr_card_5(nsigz_value, card_5, module):
+    # Note that the number of sigma zero values in card 5 should be equal to
+    # the number of sigma zero values ('nsigz_value') defined in card 2.
+    rule.card_must_be_defined('card_5', card_5, module, None)
+    stmt_iter = env.get_statement_iterator(card_5)
+    stmt_len = len(stmt_iter)
+    if stmt_len == nsigz_value:
+        for i in range(stmt_len):
+            analyze_groupr_card_5_sigz(i, env.next(stmt_iter), card_5, module)
+    else:
+        msg = ('saw ' + str(stmt_len) + ' statements in \'card_5\'' +
+               ' but expected ' + str(nsigz_value) + ' since ' +
+               'nsigz = ' + str(nsigz_value) + ' in \'card_2\', module ' +
+               '\'groupr\'.')
+        rule.semantic_error(msg, card_5)
+    return card_5
+
+def analyze_groupr_card_5_sigz(expected_index, sigz_node, card_5, module):
+    expected = ('sigz', expected_index)
+    rule.identifier_must_be_defined(expected, sigz_node, card_5, module)
+    return env.get_value(env.get_r_value(sigz_node))
