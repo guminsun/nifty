4fc53c6b1b3c1d0fc631be95b1ca36ecc57205b8
ceh <c.emil.hessman@gmail.com>
2011-05-03 13:00:41 +0200

    Added semantic checks for card 1, module groupr.

--- nifty/analyzer/analyze_groupr.py
+++ nifty/analyzer/analyze_groupr.py
@@ -5,4 +5,26 @@ import analyzer_rules as rule
 # Analyze groupr. Checks if groupr is somewhat semantically correct.
 
 def analyze_groupr(module):
+    analyze_groupr_card_list(module)
     return module
+
+def analyze_groupr_card_list(module):
+    card_iter = env.get_card_iterator(module)
+    # Card 1 must always be defined.
+    analyze_groupr_card_1(env.next(card_iter), module)
+    
+    # No more cards are allowed. The next card returned by env.next(card_iter)
+    # should be 'None'.
+    #rule.no_card_allowed(env.next(card_iter), module)
+    return module
+
+def analyze_groupr_card_1(card_1, module):
+    # Card 1 must be defined.
+    rule.card_must_be_defined('card_1', card_1, module, None)
+    stmt_iter = env.get_statement_iterator(card_1)
+    rule.analyze_unit_number('nendf', env.next(stmt_iter), card_1, module)
+    rule.analyze_unit_number('npend', env.next(stmt_iter), card_1, module)
+    rule.analyze_optional_unit_number('ngout1', env.next(stmt_iter), card_1, module)
+    rule.analyze_optional_unit_number('ngout2', env.next(stmt_iter), card_1, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card_1, module)
+    return card_1
