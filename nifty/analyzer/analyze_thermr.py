dc2f898108dde80c5bf7d0170b04d3ef3d66af16
ceh <c.emil.hessman@gmail.com>
2011-05-09 11:27:06 +0200

    Added semantic checks for card 1, module thermr.

--- nifty/analyzer/analyze_thermr.py
+++ nifty/analyzer/analyze_thermr.py
@@ -9,4 +9,16 @@ def analyze_thermr(module):
     return module
 
 def analyze_thermr_card_list(module):
-    pass
+    card_iter = env.get_card_iterator(module)
+    analyze_thermr_card_1(env.next(card_iter), module)
+    # rule.no_card_allowed(env.next(card_iter), module)
+    return module
+
+def analyze_thermr_card_1(card, module):
+    rule.card_must_be_defined('card_1', card, module, None)
+    stmt_iter = env.get_statement_iterator(card)
+    rule.analyze_unit_number('nendf', env.next(stmt_iter), card, module)
+    rule.analyze_unit_number('nin', env.next(stmt_iter), card, module)
+    rule.analyze_unit_number('nout', env.next(stmt_iter), card, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card, module)
+    return card
