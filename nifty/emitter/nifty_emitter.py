a5e91761cf9415676ae495ac92bc4f660e8466d1
ceh <c.emil.hessman@gmail.com>
2011-05-09 08:57:00 +0200

    Emitter more consistent with previous phases; making the translator module obsolete.

--- nifty/emitter/nifty_emitter.py
+++ nifty/emitter/nifty_emitter.py
@@ -1,3 +1,5 @@
+from nifty.environment import helpers as env
+
 ##############################################################################
 # Emitter. To give or send out.
 
@@ -5,43 +7,128 @@ def emit(program):
     return program_to_string(program)
 
 def program_to_string(program):
-    string = ''
-    for module in program:
+    module_list = program.get('module_list')
+    return module_list_to_string(module_list)
+
+def module_list_to_string(module_list):
+    string = str()
+    for module in module_list:
         string += module_to_string(module)
     return string
 
 def module_to_string(module):
-    string = ''
-    if module[0] == 'stop':
-        string += module[0]
-    else:
-        string += module[0] + newline() + card_list_to_string(module[1])
+    module_name = module.get('module_name')
+    string = module_name + newline()
+    card_list = module.get('card_list')
+    string += card_list_to_string(card_list)
     return string
 
 def card_list_to_string(card_list):
-    string = ''
+    string = str()
     for card in card_list:
-        string += card_to_string(card) + default() + newline()
+        string += card_to_string(card)
     return string
 
 def card_to_string(card):
-    string = ''
-    card_length = len(card)
-    for i in range(card_length):
-        if i < card_length-1:
-            string += variable_to_string(card[i]) + space()
-        else:
-            string += variable_to_string(card[i])
+    statement_list = card.get('statement_list')
+    string = statement_list_to_string(statement_list) + default() + newline()
     return string
 
-def variable_to_string(variable):
-    string = ''
-    node_type = variable[0]
-    value = variable[1]
-    if node_type == 'string':
-        string = single_quote() + str(value) + single_quote()
+def statement_list_to_string(statement_list):
+    string = str()
+    statement_list_length = len(statement_list)
+    for i in range(statement_list_length):
+        # Append a space to every statement except the last one.
+        if i < statement_list_length-1:
+            string += statement_to_string(statement_list[i]) + space()
+        else:
+            string += statement_to_string(statement_list[i])
+    return string
+
+def statement_to_string(node):
+    if env.is_assignment(node):
+        return assignment_to_string(node)
     else:
-        string = str(value)
+        # Catch anything else, just in case.
+        print('--- emitter: XXX statement not implemented yet:',
+              env.get_node_type(node))
+        return node
+
+def assignment_to_string(node):
+    l_value = node.get('l_value')
+    r_value = node.get('r_value')
+    if env.is_singleton(l_value):
+        return singleton_to_string(r_value)
+    elif env.is_pair(l_value):
+        return pair_to_string(r_value)
+    elif env.is_triplet(l_value):
+        return triplet_to_string(r_value)
+    else:
+        # Catch anything else, just in case.
+        print('--- emitter: XXX l-value node not implemented yet:',
+              env.get_node_type(node))
+        return node
+
+def singleton_to_string(node):
+    element_1 = node.get('element_1')
+    return element_to_string(element_1)
+
+def pair_to_string(node):
+    element_1 = node.get('element_1')
+    element_2 = node.get('element_2')
+    element_1_string = element_to_string(element_1)
+    element_2_string = element_to_string(element_2)
+    # Format the pair as a NJOY pair.
+    # XXX: What if element node types differ? E.g. if element_1 is a string
+    # and element_2 is an integer.
+    string = element_1_string + element_2_string
+    return string
+
+def triplet_to_string(node):
+    element_1 = node.get('element_1')
+    element_2 = node.get('element_2')
+    element_3 = node.get('element_3')
+    element_1_string = element_to_string(element_1)
+    element_2_string = element_to_string(element_2)
+    element_3_string = element_to_string(element_3)
+    # Format the triplet as a NJOY triplet.
+    # XXX: What if element node types differ? E.g. if element_1 is a string
+    # and element_2 is an integer.
+    string = element_1_string + element_2_string + element_3_string
+    return string
+
+def element_to_string(node):
+    if env.is_float(node):
+        return float_to_string(node)
+    elif env.is_integer(node):
+        return integer_to_string(node)
+    elif env.is_null(node):
+        return null_to_string(node)
+    elif env.is_string(node):
+        return string_to_string(node)
+    else:
+        # Catch anything else, just in case.
+        print('--- emitter: XXX element node not implemented yet:',
+              env.get_node_type(node))
+        return node
+
+def float_to_string(node):
+    value = node.get('value')
+    string = str(value)
+    return string
+
+def integer_to_string(node):
+    value = node.get('value')
+    string = str(value)
+    return string
+
+def null_to_string(node):
+    return ''
+
+def string_to_string(node):
+    value = node.get('value')
+    # Format the string as a NJOY string, i.e. delimited by single quotes.
+    string = single_quote() + str(value) + single_quote()
     return string
 
 ##############################################################################
