7d168cc4fabd06315ec7b4d6b9cfce8a488d15f2
ceh <c.emil.hessman@gmail.com>
2011-05-10 12:03:22 +0200

    Added semantic checks for card 2, module errorr.

--- nifty/analyzer/analyze_errorr.py
+++ nifty/analyzer/analyze_errorr.py
@@ -10,8 +10,8 @@ def analyze_errorr(module):
 
 def analyze_errorr_card_list(module):
     card_iter = env.get_card_iterator(module)
-    # Card 1 must always be defined.
     analyze_errorr_card_1(env.next(card_iter), module)
+    card_2, ign = analyze_errorr_card_2(env.next(card_iter), module)
     # XXX: rule.no_card_allowed(env.next(card_iter), module)
 
 def analyze_errorr_card_1(card, module):
@@ -25,3 +25,77 @@ def analyze_errorr_card_1(card, module):
     rule.analyze_optional_unit_number('nstan', env.next(stmt_iter), card, module)
     rule.no_statement_allowed(env.next(stmt_iter), card, module)
     return card
+
+def analyze_errorr_card_2(card, module):
+    rule.card_must_be_defined('card_2', card, module, None)
+    stmt_iter = env.get_statement_iterator(card)
+    rule.analyze_material('matd', env.next(stmt_iter), card, module)
+    ign = analyze_errorr_card_2_ign(env.next(stmt_iter), card, module)
+    iwt = analyze_errorr_card_2_iwt(env.next(stmt_iter), card, module)
+    analyze_errorr_card_2_iprint(env.next(stmt_iter), card, module)
+    analyze_errorr_card_2_irelco(env.next(stmt_iter), card, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card, module)
+    return card, ign
+
+def analyze_errorr_card_2_ign(node, card, module):
+    # Neutron group option (ign) does not have to be specified, defaults to 1.
+    if node is None:
+        return 1
+    else:
+        l_value, r_value = rule.analyze_singleton(node, card, module)
+        rule.identifier_must_be_defined('ign', l_value, card, module)
+        ign = rule.must_be_int(l_value, r_value, card, module)
+        # XXX: Additional checks? Range?
+        return ign
+
+def analyze_errorr_card_2_iwt(node, card, module):
+    # Weight function option (iwt) does not have to be specified, defaults to
+    # 6.
+    if node is None:
+        return 6
+    else:
+        l_value, r_value = rule.analyze_singleton(node, card, module)
+        rule.identifier_must_be_defined('iwt', l_value, card, module)
+        iwt = rule.must_be_int(l_value, r_value, card, module)
+        # XXX: Additional checks? Range?
+        return iwt
+
+def analyze_errorr_card_2_iprint(node, card, module):
+    # iprint (0 = min, 1 = max) does not have to be defined, defaults to 1
+    # meaning maximum print option.
+    if node is None:
+        return 1
+    else:
+        l_value, r_value = rule.analyze_singleton(node, card, module)
+        rule.identifier_must_be_defined('iprint', l_value, card, module)
+        iprint = rule.must_be_int(l_value, r_value, card, module)
+        if iprint not in range(0,2):
+            id_name = l_value.get('name')
+            card_name = card.get('card_name')
+            module_name = module.get('module_name')
+            msg = ('illegal print option in \'' + card_name + '\', module ' +
+                   ' \'' + module_name + '\': ' + id_name + ' = ' +
+                   str(iprint) + ', expected 0 for min, 1 for max ' + 
+                   '(default = 1).')
+            rule.semantic_error(msg, node)
+        return iprint
+
+def analyze_errorr_card_2_irelco(node, card, module):
+    # Type of covariances does not have to be defined, defaults to 1 meaning
+    # relative covariances.
+    if node is None:
+        return 1
+    else:
+        l_value, r_value = rule.analyze_singleton(node, card, module)
+        rule.identifier_must_be_defined('irelco', l_value, card, module)
+        irelco = rule.must_be_int(l_value, r_value, card, module)
+        if irelco not in range(0,2):
+            id_name = l_value.get('name')
+            card_name = card.get('card_name')
+            module_name = module.get('module_name')
+            msg = ('illegal type of covariance in \'' + card_name + '\',' +
+                   ' module \'' + module_name + '\': ' + id_name + ' = ' +
+                   str(irelco) + ', expected 0 for absolute or 1 for relative' +
+                   ' (default = 1).')
+            rule.semantic_error(msg, node)
+    return irelco
