af05cd6d8411db7f70269591ae7bbf28501cb86c
ceh <c.emil.hessman@gmail.com>
2011-05-10 13:36:55 +0200

    Added semantic checks for card 3, module errorr.

--- nifty/analyzer/analyze_errorr.py
+++ nifty/analyzer/analyze_errorr.py
@@ -10,8 +10,14 @@ def analyze_errorr(module):
 
 def analyze_errorr_card_list(module):
     card_iter = env.get_card_iterator(module)
-    analyze_errorr_card_1(env.next(card_iter), module)
+    card_1, ngout = analyze_errorr_card_1(env.next(card_iter), module)
     card_2, ign = analyze_errorr_card_2(env.next(card_iter), module)
+    # Card 3 should only be defined for ngout = 0.
+    if ngout == 0:
+        analyze_errorr_card_3(env.next(card_iter), module)
+    # XXX: The rest of the cards depends on the ENDF file version which is
+    # being used as input. The translator cannot check this as of yet, hence,
+    # just pass the cards along, skipping semantic analysis for them.
     # XXX: rule.no_card_allowed(env.next(card_iter), module)
 
 def analyze_errorr_card_1(card, module):
@@ -19,12 +25,12 @@ def analyze_errorr_card_1(card, module):
     stmt_iter = env.get_statement_iterator(card)
     rule.analyze_unit_number('nendf', env.next(stmt_iter), card, module)
     rule.analyze_unit_number('npend', env.next(stmt_iter), card, module)
-    rule.analyze_optional_unit_number('ngout', env.next(stmt_iter), card, module)
+    ngout = rule.analyze_optional_unit_number('ngout', env.next(stmt_iter), card, module)
     rule.analyze_optional_unit_number('nout', env.next(stmt_iter), card, module)
     rule.analyze_optional_unit_number('nin', env.next(stmt_iter), card, module)
     rule.analyze_optional_unit_number('nstan', env.next(stmt_iter), card, module)
     rule.no_statement_allowed(env.next(stmt_iter), card, module)
-    return card
+    return card, ngout
 
 def analyze_errorr_card_2(card, module):
     rule.card_must_be_defined('card_2', card, module, None)
@@ -99,3 +105,28 @@ def analyze_errorr_card_2_irelco(node, card, module):
                    ' (default = 1).')
             rule.semantic_error(msg, node)
     return irelco
+
+def analyze_errorr_card_3(card, module):
+    rule.card_must_be_defined('card_3', card, module, None)
+    stmt_iter = env.get_statement_iterator(card)
+    # XXX: Clean print, make it general and put it in the rules module?
+    analyze_errorr_card_3_mprint(env.next(stmt_iter), card, module)
+    rule.analyze_temperature('tempin', env.next(stmt_iter), card, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card, module)
+    return card
+
+def analyze_errorr_card_3_mprint(node, card, module):
+    # Does mprint have to be defined?
+    l_value, r_value = rule.analyze_singleton(node, card, module)
+    rule.identifier_must_be_defined('mprint', l_value, card, module)
+    mprint = rule.must_be_int(l_value, r_value, card, module)
+    if mprint not in range(0,2):
+        id_name = l_value.get('name')
+        card_name = card.get('card_name')
+        module_name = module.get('module_name')
+        msg = ('illegal print option for group averaging in \'' + card_name +
+               '\', module \'' + module_name + '\': ' + id_name + ' = ' +
+               str(mprint) + ', expected 0 for min, 1 for max ' +
+               '(default = 1).')
+        rule.semantic_error(msg, node)
+    return mprint
