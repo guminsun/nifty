6c7af39f980a76243d85630e01ead88e16f02652
ceh <c.emil.hessman@gmail.com>
2011-05-11 10:51:09 +0200

    Added semantic checks for card 0, module plotr.

--- nifty/analyzer/analyze_plotr.py
+++ nifty/analyzer/analyze_plotr.py
@@ -9,4 +9,15 @@ def analyze_plotr(module):
     return module
 
 def analyze_plotr_card_list(module):
-    pass
+    card_iter = env.get_card_iterator(module)
+    analyze_plotr_card_0(env.next(card_iter), module)
+    # XXX: rule.no_card_allowed(env.next(card_iter), module)
+    return module
+
+def analyze_plotr_card_0(card, module):
+    rule.card_must_be_defined('card_0', card, module, None)
+    stmt_iter = env.get_statement_iterator(card)
+    rule.analyze_unit_number('nplt', env.next(stmt_iter), card, module)
+    rule.analyze_optional_unit_number('nplt0', env.next(stmt_iter), card, module)
+    rule.no_statement_allowed(env.next(stmt_iter), card, module)
+    return card    
