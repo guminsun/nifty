9ba219281cadd15be25275fbd43dfe4ebf55221a
ceh <c.emil.hessman@gmail.com>
2011-05-18 08:45:17 +0200

    Added organization of card 0, module plotr.

--- nifty/organizer/organize_plotr.py
+++ nifty/organizer/organize_plotr.py
@@ -14,4 +14,19 @@ def organize_plotr(module):
 
 def organize_card_list(card_list, module):
     card_iter = env.get_card_iterator(module)
+    # Card 1 should always be defined.
+    organize_card_0(env.next(card_iter), module)
+    # No more cards are allowed. The next card returned by env.next(card_iter)
+    # should be 'None'.
+    # XXX: rule.no_card_allowed(env.next(card_iter), module)
     return card_list
+
+def organize_card_0(card, module):
+    # Card 0 must be defined. OrganizeError is raised if 'card' is not card 0
+    # (the original syntax tree will be returned).
+    helper.card_must_be_defined('card_0', card)
+    expected_map = {
+        0 : ('singleton', 'identifier', ('nplt', None)),
+        1 : ('singleton', 'identifier', ('nplt0', 0)),
+    }
+    return helper.organize_card(expected_map, card)
