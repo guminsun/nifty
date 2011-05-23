1e525d00c660ef8d0e1414a79b72e02f29351630
ceh <c.emil.hessman@gmail.com>
2011-05-23 11:21:10 +0200

    Added organization for module thermr.

--- nifty/organizer/organize_thermr.py
+++ nifty/organizer/organize_thermr.py
@@ -38,13 +38,40 @@ def card_dummy(card):
     return card
 
 def organize_card_1(card, module):
-    pass
+    expected_map = {
+        0 : ('identifier', ('nendf', None)),
+        1 : ('identifier', ('nin', None)),
+        2 : ('identifier', ('nout', None)),
+    }
+    return helper.organize_card(expected_map, card)
 
 def organize_card_2(card, module):
-    pass
+    expected_map = {
+        0 : ('identifier', ('matde', None)),
+        1 : ('identifier', ('matdp', None)),
+        2 : ('identifier', ('nbin', None)),
+        3 : ('identifier', ('ntemp', None)),
+        4 : ('identifier', ('iinc', None)),
+        5 : ('identifier', ('icoh', None)),
+        6 : ('identifier', ('natom', None)),
+        7 : ('identifier', ('mtref', None)),
+        8 : ('identifier', ('iprint', 0)),
+    }
+    return helper.organize_card(expected_map, card)
 
 def organize_card_3(card, module):
-    pass
+    card_2 = env.get_card('card_2', module)
+    ntemp = helper.get_identifier_value('ntemp', card_2)
+    if ntemp is None:
+        organize_error()
+    expected_map = {}
+    for i in range(ntemp):
+        expected_map[i] = ('array', ('tempr', None, i))
+    return helper.organize_card(expected_map, card)
 
 def organize_card_4(card, module):
-    pass
+    expected_map = {
+        0 : ('identifier', ('tol', None)),
+        1 : ('identifier', ('emax', None)),
+    }
+    return helper.organize_card(expected_map, card)
